0d5b1976e0df7063f2a8f454e646b1456877aa5f
PeretzBatel <peretzbatel123@gmail.com>
2023-04-02 17:50:02 +0300

    initial commit

--- main.go
+++ main.go
@@ -5,3 +5,20 @@ func main() { // Has to be first matching package main
 	cards.shuffle()
 	cards.print()
 }
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Two of Clubs" {
+		t.Errorf("Expected last card of Two of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
